3cfbed5a3617d9cde5a558e213bba6028506ff43
Jonathan Ferguson <jono@spiralarm.com>
2009-10-20 07:41:26 +0000

    Handling 400 and 500s we now have a site map and lift rules for uri not found and site map failure and for good measure and exception handler.
    git-svn-id: https://svn.spiralarm.com/spiralarm/spiralarm.com/trunk@5358 762679c8-bba8-42f4-ac16-b637b3357dba

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -7,7 +7,7 @@ import _root_.net.liftweb.sitemap.Loc._
 import Helpers._
 import _root_.net.liftweb.mapper.{DB, ConnectionManager, Schemifier, DefaultConnectionIdentifier, ConnectionIdentifier}
 import _root_.java.sql.{Connection, DriverManager}
-import _root_.com.spiralarm.www.model._
+
 import _root_.javax.servlet.http.{HttpServletRequest}
 
 /**
@@ -16,8 +16,34 @@ import _root_.javax.servlet.http.{HttpServletRequest}
   */
 class Boot {
   def boot {
+    Log.info("RUN MODE: "+Props.mode+" on "+Props.userName+"@"+Props.hostName)
+
     // where to search snippet
     LiftRules.addToPackages("com.spiralarm.www")
+
+    // Build SiteMap
+	val pages = Menu(Loc("Home", List("index"), "Home",Hidden)) ::
+            Menu(Loc("aboutSpiralArm", List("aboutSpiralArm"), "aboutSpiralArm", Hidden)) ::
+            Menu(Loc("contactUs", List("contactUs"), "contactUs", Hidden)) ::
+            Menu(Loc("howWeWork", List("howWeWork"), "howWeWork", Hidden)) ::
+            Menu(Loc("privacyStatement", List("privacyStatement"), "privacyStatement", Hidden)) ::
+            Menu(Loc("siteMap", List("siteMap"), "siteMap", Hidden)) ::
+            Menu(Loc("siteReorganized", List("siteReorganized"), "siteReorganized", Hidden)) ::
+            Menu(Loc("technologies", List("technologies"), "technologies", Hidden)) ::
+            Menu(Loc("work", List("work"), "work", Hidden)) ::
+            Menu(Loc("newMobileImagingService", List("ourWork" , "newMobileImagingService"), "newMobileImagingService", Hidden)) ::   
+            Menu(Loc("pictureMessagingAtTheGuardian", List("ourWork" , "pictureMessagingAtTheGuardian"), "pictureMessagingAtTheGuardian", Hidden)) ::   
+            Menu(Loc("pictureMessagingAtTheTimes", List("ourWork" , "pictureMessagingAtTheTimes"), "pictureMessagingAtTheTimes", Hidden)) ::   
+            Menu(Loc("regionalNewspapersEmbraceMMS", List("ourWork" , "regionalNewspapersEmbraceMMS"), "regionalNewspapersEmbraceMMS", Hidden)) ::   
+            Menu(Loc("visualizingTvResearch", List("ourWork" , "visualizingTvResearch"), "visualizingTvResearch", Hidden)) ::   
+            Menu(Loc("websiteRedevelopment", List("ourWork" , "websiteRedevelopment"), "websiteRedevelopment", Hidden)) ::   
+            Nil    
+    
+    LiftRules.setSiteMap(SiteMap(pages:_*))
+    LiftRules.siteMapFailRedirectLocation = "/siteReorganized.html" :: Nil
+    LiftRules.uriNotFound.prepend{ case (req, _) => PermRedirectResponse("/siteReorganized.html", req)}  
+    LiftRules.exceptionHandler.prepend{ case(mode,req, throwable) =>PermRedirectResponse("siteReorganized", req)}
+
   }
 
 }
