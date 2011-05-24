461317d0f4fb3dd78e962952ddabf94295f4b138
Jonathan Ferguson <Jono@spiralarm.com>
2011-05-24 11:54:09 +1000

    code coverage

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,5 +1,8 @@
  import sbt._
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
 
-	lazy val eclipse = "de.element34" % "sbt-eclipsify" % "0.7.0"
+	lazy val scctRepo = "scct-repo" at "http://mtkopone.github.com/scct/maven-repo/"
+    lazy val scctPlugin = "reaktor" % "sbt-scct-for-2.8" % "0.1-SNAPSHOT"
+
+    lazy val eclipse = "de.element34" % "sbt-eclipsify" % "0.7.0"
 }
