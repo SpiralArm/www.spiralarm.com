5bf8e0105a0d72fe4acfbb4863ae1f21e306cbee
Jonathan Ferguson <Jono@spiralarm.com>
2011-05-24 12:54:50 +1000

    Added cloud bees plugin.

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -1,9 +1,12 @@
 import sbt._
 import de.element34.sbteclipsify._
 
-class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) with Eclipsify {
+class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) with Eclipsify  with bees.RunCloudPlugin {
   val liftVersion = "2.3"
 
+  override def beesUsername = Some("spiralarm")
+  override def beesApplicationId = Some("spiralarm/spiralarm.com")	  
+	  
   // uncomment the following if you want to use the snapshot repo
   //  val scalatoolsSnapshot = ScalaToolsSnapshots
 
@@ -16,7 +19,10 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) with Eclips
     "net.liftmodules" %% "google-analytics" % "0.8",
     
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test",
-    "ch.qos.logback" % "logback-classic" % "0.9.26"
+    "ch.qos.logback" % "logback-classic" % "0.9.26",
 
+    "junit" % "junit" % "4.5" % "test",
+    "org.scala-tools.testing" %% "specs" % "1.6.6" % "test"
+    
   ) ++ super.libraryDependencies
 }
