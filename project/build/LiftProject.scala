0ac925b9b94675c2b55af35a0d086bcc279aca22
Richard Dallaway <richard@dallaway.com>
2011-08-05 11:56:27 +0100

    Upgrade to 2.4-M3.

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -2,18 +2,20 @@ import sbt._
 import de.element34.sbteclipsify._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) with Eclipsify  with bees.RunCloudPlugin {
-  val liftVersion = "2.4-M2"
+ 
+  val liftVersion = "2.4-M3"
 
   override def beesUsername = Some("spiralarm")
   override def beesApplicationId = Some("spiralarm/spiralarm.com")	  
 	  
   // uncomment the following if you want to use the snapshot repo
   //  val scalatoolsSnapshot = ScalaToolsSnapshots
-
-  val liftModulesRelease = "liftmodules repository" at "http://repository-liftmodules.forge.cloudbees.com/release/"
-
   override def jettyWebappPath = webappPath
   override def scanDirectories = Nil
+
+  lazy val liftModulesRelease = "liftmodules repository" at "http://repository-liftmodules.forge.cloudbees.com/release/"
+  lazy val JavaNet = "Java.net Maven2 Repository" at "http://download.java.net/maven/2/"
+
 
   override def libraryDependencies = Set(
     "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default" withSources(),
